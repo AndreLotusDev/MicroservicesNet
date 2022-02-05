8276f4c5abedfb21a386416a6c5fcfcfb6fce7bc
Andre Soares Gomes <andrsoares953@yahoo.com>
2022-02-05 01:01:44 -0300

    Implemented irepository pattern and mongodb driver

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var clientConnection = new MongoClient(configuration.GetValue<string>("ConnectionStrings:MongoShopping"));
+            var database = clientConnection.GetDatabase(configuration.GetValue<string>("ConnectionStrings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("ConnectionStrings:CollectionName"));
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
